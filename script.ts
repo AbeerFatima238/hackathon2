3083610333831b68449bec291d348a44fbb0c087
AbeerFatima238 <abeerfatimahom2@gmail.com>
2024-11-11 03:20:36 +0500

    change

--- script.ts
+++ script.ts
@@ -24,8 +24,8 @@ formm.addEventListener('submit',(event: Event)=>{
     <h2><b>Resume</b></h2>
     <h3>Personal Information</h3>
     <p><b>Name:</b>${name}</p>
-    <p><b>Name:</b>${email}</p>
-    <p><b>Name:</b>${phone}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Number:</b>${phone}</p>
 
     <h3>Education</h3>
     <p>${education}</p>
